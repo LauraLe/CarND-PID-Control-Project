f6fc652e98c269ad9aff4d796449466789a37bdb
Trang Le <trangle.laura@gmail.com>
2018-08-07 23:48:04 -0400

    test Commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,8 @@
 #include "PID.h"
+#include <math>
+#include <iostream>
+#include <uWS/uWS.h>
+#include <numeric>
 
 using namespace std;
 
@@ -11,11 +15,80 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    p[0] = Kp;
+    p[1] = Ki;
+    p[2] = Kd;
+    
+    //Initialize errors
+    prev_cte = 0;
+    twiddle_error = 0;
+    err[0] = 0;
+    err[1] = 0;
+    err[2] = 0;
+    
+    //optimizer timestemp
+    optimizer_t = 0;
+    
 }
 
 void PID::UpdateError(double cte) {
+    if (prev_cte == 0) prev_cte = cte;
+    err[0] = cte ; // p_error;
+    err[1] += cte ; // i_error
+    err[2] = cte - prev_cte; // d_error
+    
+    prev_cte = cte;
 }
 
 double PID::TotalError() {
+    double total_error = pow(err[0],2);
+    return total_error;
 }
 
+double PID::CalculateSteer(){
+    double steer;
+    double max_steer = 1.0;
+    double min_steer = -1.0;
+    
+    steer = -p[0] * err[0] - p[1]*err[1] - p[2] *err[2];
+    
+    if(steer > max_steer) steer = max_steer;
+    if(steer < min_steer) steer = min_steer;
+    
+    return steer;
+}
+
+double PID::CalculateThrottle(double speed, <#double steer#>){
+    double throttle;
+    
+    if (is_twiddle and (optimizer_t < 10)){ //if just start the optimizer, remove throttle to avoid noise to steering
+        throttle = 0.0;
+    } else{
+        throttle = 0.3;
+    }
+    
+    return throttle;
+}
+
+void PID::Twiddle(){
+    if (is_twiddle){
+        if(optimizer_t == start_measure){
+            twiddle_error = TotalError(); //start calculate twiddle error
+        }
+        if (optimizer_t > start_measure){
+            twiddle_error += TotalError(); // while between start_measure and end_measure, accumulate error
+        }
+        
+        if (optimizer_t > end_measure){
+             cout << " optimizer measure error end with total error " << twiddle_error <<endl;
+            
+            double sum_dp = dp[0] + dp[1] + dp[2];{
+                
+            }
+            if ((fabs(sum_dp) > twiddle_tol) & (twiddle_iter < max_iters)){
+                
+            }
+            
+        }
+    }
+}
--- src/PID.h
+++ src/PID.h
@@ -9,6 +9,9 @@ public:
   double p_error;
   double i_error;
   double d_error;
+  double prev_cte;
+    double twiddle_error;
+   double err[3];
 
   /*
   * Coefficients
@@ -16,6 +19,29 @@ public:
   double Kp;
   double Ki;
   double Kd;
+  double p[3];
+ 
+ /*
+  * Twiddle Parameters
+  */
+    const int start_measure = 300;         // starts measuring error for twiddle
+    const int end_measure = 700;           // ending measuring error for twiddle
+    const double twiddle_tol = 0.2;   // Twiddle weights tolerance
+    const int max_iters = 5000;           // Maximum number of iterations
+    const int num_params = 3;
+    
+    double dp[3];
+    double best_p[3];
+    double best_error;
+    
+    bool is_twiddle = false;
+    bool is_retrial = false;
+    
+    int optimizer_t;
+    int twiddle_pass;
+    int twiddle_iter;
+    // server
+    uWS::WebSocket<uWS::SERVER> server;
 
   /*
   * Constructor
@@ -41,6 +67,37 @@ public:
   * Calculate the total PID error.
   */
   double TotalError();
+    
+  /*
+   * Calculate the steering angle
+   */
+    double CalculateSteer();
+    
+    /*
+     * Calculate Throttle given Steer Value & speed
+     */
+    double CalculateThrottle(double speed, double steer);
+    
+    /*
+     * Initializes twiddle parameters.
+     */
+    void InitializeTwiddle();
+    
+    /*
+     * Optimizes parameters (Kp, Ki, Kd).
+     */
+    void Twiddle();
+    
+    /*
+     * Restarts simulator.
+     */
+    void RestartSim(uWS::WebSocket<uWS::SERVER> ws);
+    
+    /*
+     * Stores server info.
+     */
+    void SetServer(uWS::WebSocket<uWS::SERVER> ws);
+    
 };
 
 #endif /* PID_H */
